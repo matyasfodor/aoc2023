daeaa2a7935c7b9bbc40e3d749253fb5e3efc50f
Matyas Fodor <matyas.fodor@gmail.com>
2023-12-07 10:28:55 +0100

    feat: day 5 second half

--- src/d05/main.ts
+++ src/d05/main.ts
@@ -67,7 +67,7 @@ const parse = (fname: string): Alamanc => {
 };
 
 
-const alamanc = parse("src/d05/test.txt");
+const alamanc = parse("src/d05/input.txt");
 
 // console.log(`Almanc ${JSON.stringify(alamanc, null, 2)}}`);
 
@@ -94,6 +94,10 @@ const first = (alamanc: Alamanc): number => {
   }, Infinity);
 };
 
+const rangesSum = (ranges: [number, number][]): number => {
+  return ranges.reduce((sum, [from, to]) => sum + to - from, 0);
+};
+
 const second = (alamanc: Alamanc): number => {
   const pairs = Array.from(
     { length: alamanc.seeds.length / 2 },
@@ -104,23 +108,52 @@ const second = (alamanc: Alamanc): number => {
   );
 
   const finalRanges = alamanc.maps.reduce((ranges: [number, number][], map) => {
-    return ranges.flatMap(([from, to]) => {
+    // console.log(`ranges: ${JSON.stringify(ranges)}`);
+    const newRanges = ranges.flatMap(([from, to]) => {
+      const originalRange: [number, number] = [from, to];
       const newRanges: [number, number][] = [];
       map.rules.forEach(({ sourceStart, destinationStart, length, shift }) => {
-        if (from < to && from < sourceStart) {
-          const increment = Math.min(sourceStart, to) - from;
+        const sourceEnd = sourceStart + length;
+        if (from >= to) {
+          return;
         }
-        if (from < to && sourceStart <= from && from <= sourceStart + length) {
-          const increment = Math.min(sourceStart + length, to) - from;
-          newRanges.push([destinationStart, destinationStart + increment]);
-          from += increment;
+        // There is an overlap
+        if (to > sourceStart && from < sourceEnd) {
+          if (from < sourceStart) {
+            newRanges.push([from, sourceStart]);
+            from = sourceStart;
+          }
+          const startOffset = from - sourceStart;
+          const overlapSize = Math.min(to, sourceEnd) - from;
+          // console.log(`overlapSize: ${overlapSize}, from: ${from}, to: ${to}, sourceStart: ${sourceStart}, sourceEnd: ${sourceEnd}`);
+          newRanges.push([destinationStart + startOffset, destinationStart + startOffset + overlapSize]);
+          from += overlapSize;
+          // if (to < sourceEnd) {
+          //   newRanges.push([from + shift, to + shift]);
+          //   from = to;
+          // }
         }
+      //   if (from < to && from < sourceStart) {
+      //     const increment = Math.min(sourceStart, to) - from;
+      //   }
+      //   if (from < to && sourceStart <= from && from <= sourceStart + length) {
+      //     const increment = Math.min(sourceStart + length, to) - from;
+      //     newRanges.push([destinationStart, destinationStart + increment]);
+      //     from += increment;
+      //   }
       });
+      if (from < to) {
+        newRanges.push([from, to]);
+      }
+      // console.log(`originalRange: ${originalRange} - ${rangesSum([originalRange])} newRanges: ${JSON.stringify(newRanges)} - rangesSum: ${rangesSum(newRanges)}`);
       return newRanges;
     });
+
+    // console.log(`#### newRanges: ${JSON.stringify(newRanges)} - rangesSum: ${rangesSum(newRanges)}`);
+    return newRanges;
   }, pairs);
 
-  console.log(`finalRanges: ${finalRanges}`);
+  // console.log(`finalRanges: ${JSON.stringify(finalRanges)}`);
   return finalRanges.reduce((min, [from, to]) => {
     return from < min ? from : min;
   }, Infinity);
