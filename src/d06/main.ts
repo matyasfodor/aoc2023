993158c63593b44b14c4bcb489c74b15f1d6143e
Matyas Fodor <matyas.fodor@gmail.com>
2023-12-07 15:17:00 +0100

    d o6 optimized

--- src/d06/main.ts
+++ src/d06/main.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { get } from 'http';
 
 const input = fs.readFileSync('./src/d06/input.txt', 'utf8');
 
@@ -10,16 +11,90 @@ const timesAndDistances = input.split('\n').map((line) => {
 
 const transposed = timesAndDistances[0].map((_, colIndex): [number, number] => [timesAndDistances[0][colIndex], timesAndDistances[1][colIndex]]);
 
+const getDistance = (time: number, pressingTime: number): number => {
+  const velocity = pressingTime;
+  const remainingTime = time - pressingTime;
+  return velocity * remainingTime;
+};
+
 const getVariations = (time: number, distance: number): number => {
-  return Array.from({ length: time }, (_, i) => {
+  const valuesAbove = Array.from({ length: time }, (_, i) => {
     const velocity = i;
-    const remainingTime = time - i;
-    return velocity * remainingTime;
-  }).filter((v) => distance < v).length;
+    const currentDistance = getDistance(time, velocity);
+    return {distance: currentDistance, i};
+  }).filter(({distance: v}) => distance < v);
+  // console.log(`Values above ${JSON.stringify(valuesAbove)}`);
+  console.log(`Low: ${valuesAbove[0].i}, High: ${valuesAbove[valuesAbove.length - 1].i}`);
+  return valuesAbove.length;
+};
+
+const binarySearch = (lowerBound: number, upperBound: number, condition: (currentValue: number) => number): number => {
+  let lower = lowerBound;
+  let upper = upperBound;
+  let currentValue = Math.floor((upper + lower) / 2);
+  while (lower < upper) {
+    const result = condition(currentValue);
+    if (result === 0) {
+      return currentValue;
+    }
+    if (result < 0) {
+      lower = currentValue + 1;
+    }
+    if (result > 0) {
+      upper = currentValue - 1;
+    }
+    // console.log(lower, upper);
+    currentValue = Math.floor((upper + lower) / 2);
+  }
+  return currentValue;
+};
+
+const getVariations2 = (time: number, distance: number): number => {
+  // let lowerLower = 0;
+  // let upperUper = time;
+  // let lowerLimit = lowerLower;
+  // while (!(getDistance(time, lowerLimit) < distance && distance < getDistance(time, lowerLimit+1))) {
+  //   // lowerLimit++;
+  // }
+  // const upperLimit = lowerLimit + 1;
+  const condition1 = (currentValue: number) => {
+    const lowerValue = getDistance(time, currentValue);
+    const upperValue = getDistance(time, currentValue + 1);
+    if (lowerValue < distance && upperValue <= distance) {
+      return -1;
+    }
+    if (lowerValue <= distance && distance < upperValue) {
+      return 0;
+    }
+    if (distance < lowerValue && distance < upperValue) {
+      return 1
+    }
+    throw new Error(`C1 No condition met for ${currentValue} distance: ${distance} lower: ${lowerValue} upper: ${upperValue}`);
+  };
+  const lowerLimit = binarySearch(0, time, condition1);
+
+  const condition2 = (currentValue: number) => {
+    const lowerValue = getDistance(time, currentValue);
+    const upperValue = getDistance(time, currentValue + 1);
+    if (upperValue < distance && lowerValue <= distance) {
+      return 1;
+    }
+    if (upperValue <= distance && distance < lowerValue) {
+      return 0;
+    }
+    if (distance < upperValue && distance < lowerValue) {
+      return -1
+    }
+    return 1;
+  }
+
+  const upperLimit = binarySearch(0, time, (value) => condition2(value));
+  console.log(lowerLimit, upperLimit);
+  return upperLimit - lowerLimit;
 };
 
 const result = transposed.reduce((acc, [time, distance]) => {
-  return acc * getVariations(time, distance);
+  return acc * getVariations2(time, distance);
 }, 1);
 
 console.log(result);
