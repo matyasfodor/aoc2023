47f7d2cf051a3c59530cb14e6ee0b582447602e5
Matyas Fodor <matyas.fodor@gmail.com>
2023-12-10 12:53:42 +0100

    d09 second

--- src/d09/main.ts
+++ src/d09/main.ts
@@ -37,8 +37,6 @@ const getNext = (rule: Rule): number => {
   // console.log(mx);
   for (let i = 1; i < rule.firstLineLength + 1; i++) {
     for (let j = rule.lineStarts.length-1; 0 <= j ; j--) {
-      // mx[i][j] = mx[i - 1][j] + mx[i][j - 1];
-      // mx[j][i] = mx[j - 1][i] + mx[j][i - 1];
       mx[j][i] = mx[j][i - 1] + (j === rule.lineStarts.length-1 ? 0 : mx[j + 1][i-1]);
     }
   }
@@ -47,9 +45,27 @@ const getNext = (rule: Rule): number => {
   return newItem;
 };
 
+const getPrev = (rule: Rule): number => {
+  const mx = Array.from({ length: rule.lineStarts.length + 1 }, (_, i) =>
+    Array.from({ length: 2 }, (_, j) =>
+      j === 1 ? rule.lineStarts[i] ?? 0 : 0
+    )
+  );
+  for (let i = mx.length - 2; 0 <= i; i--) {
+    mx[i][0] = mx[i][1] - mx[i + 1][0];
+  }
+  return mx[0][0];
+};
+
 const first = (values: number[][]): number => {
   const rules = values.map((line) => extractRule(line));
   return rules.reduce((acc, rule) => acc + getNext(rule), 0);
 };
 
-console.log(`First ${first(values)}`);
+const second = (values: number[][]): number => {
+  const rules = values.map((line) => extractRule(line));
+  return rules.reduce((acc, rule) => acc + getPrev(rule), 0);
+}
+
+// console.log(`First ${first(values)}`);
+console.log(`Second ${second(values)}`);
