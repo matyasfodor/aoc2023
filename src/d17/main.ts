bbf4458f0a79dc4f330e96b01169feb55de29ecd
Matyas Fodor <matyas.fodor@gmail.com>
2023-12-17 18:30:45 +0100

    d17 fixed

--- src/d17/main.ts
+++ src/d17/main.ts
@@ -1,98 +1,229 @@
-import fs from 'fs';
+import fs from "fs";
 
-const input = fs.readFileSync('./src/d17/test.txt', 'utf8');
+const input = fs.readFileSync("./src/d17/input.txt", "utf8");
 
 const parse = (input: string): number[][] =>
-  input.split('\n').map((line) => line.split('').map((v) => parseInt(v)));
+  input.split("\n").map((line) => line.split("").map((v) => parseInt(v)));
 
-interface PointState {
-  N?: number;
-  E?: number;
-  S?: number;
-  W?: number;
+interface PointDirectionState {
+  value: number;
+  trace: string;
 }
+interface PointState {
+  N?: PointDirectionState;
+  E?: PointDirectionState;
+  S?: PointDirectionState;
+  W?: PointDirectionState;
+}
+
+const repeat = (n: number, s: string): string => {
+  return Array.from({ length: n }, () => s).join("");
+};
+
+const traceToCorrds = (trace: string): [number, number][] => {
+  const ret: [number, number][] = [[0, 0]];
+  for (const char of trace) {
+    const [x, y] = ret[ret.length - 1];
+    switch (char) {
+      case "N":
+        ret.push([x, y - 1]);
+        break;
+      case "E":
+        ret.push([x + 1, y]);
+        break;
+      case "S":
+        ret.push([x, y + 1]);
+        break;
+      case "W":
+        ret.push([x - 1, y]);
+        break;
+    }
+  }
+  return ret;
+};
+
+const printTrace = (map: number[][], coords: [number, number][]) => {
+  const mapCopy = map.map((line) => line.map((v) => `${v}`));
+  for (const [x, y] of coords) {
+    mapCopy[y][x] = "▯";
+  }
+  console.log(mapCopy.map((line) => line.join("")).join("\n"));
+};
 
 const first = (map: number[][]): number => {
   const traces: PointState[][] = map.map((line) => line.map(() => ({})));
   const fringe = new Set(["0,0"]);
-  traces[0][0] = {S: 0, E: 0};
+  traces[0][0] = {
+    S: {
+      value: 0,
+      trace: "",
+    },
+    E: {
+      value: 0,
+      trace: "",
+    },
+  };
+  // const fringeMap = new Map<string, string>([["0,0,S", ""], ["0,0,E", ""]]);
+
   let prev = "";
   while (fringe.size > 0) {
-    // console.log(traces)
     const firstElem = [...fringe][0];
     fringe.delete(firstElem);
-    const [x, y] = firstElem.split(',').map((v) => parseInt(v));
+    const [x, y] = firstElem.split(",").map((v) => parseInt(v));
     const state = traces[y][x];
-    // console.log(x, y, state);
     if (state.E !== undefined) {
-      let value = state.E;
-      // console.log('E', value);
+      let { value, trace: currentTrace } = state.E;
       for (let i = x + 1; i < Math.min(map[y].length, x + 4); i++) {
         value += map[y][i];
-        if (traces[y][i].N === undefined || value < traces[y][i].N!) {
-          traces[y][i].N = value;
+        const currentStringified = JSON.stringify(traces[y][i]);
+        traces[y][i] = {
+          ...traces[y][i],
+          N:
+            value < (traces[y][i].N?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(i - x, "E"),
+                }
+              : traces[y][i].N,
+          S:
+            value < (traces[y][i].S?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(i - x, "E"),
+                }
+              : traces[y][i].S,
+        };
+        if (currentStringified !== JSON.stringify(traces[y][i])) {
           fringe.add(`${i},${y}`);
-        }
-        if (traces[y][i].S === undefined || value < traces[y][i].S!) {
-          traces[y][i].S = value;
-          fringe.add(`${i},${y}`);
-        
         }
       }
     }
     if (state.W !== undefined) {
-      let value = state.W;
-      for (let i = x - 1; i >= Math.max(0, x - 4); i--) {
+      let { value, trace: currentTrace } = state.W;
+      for (let i = x - 1; i > Math.max(-1, x - 4); i--) {
         value += map[y][i];
-        if (traces[y][i].N === undefined || value < traces[y][i].N!) {
-          traces[y][i].N = value;
-          fringe.add(`${i},${y}`);
-        }
-        if (traces[y][i].S === undefined || value < traces[y][i].S!) {
-          traces[y][i].S = value;
+        const currentStringified = JSON.stringify(traces[y][i]);
+        traces[y][i] = {
+          ...traces[y][i],
+          N:
+            value < (traces[y][i].N?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(x - i, "W"),
+                }
+              : traces[y][i].N,
+          S:
+            value < (traces[y][i].S?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(x - i, "W"),
+                }
+              : traces[y][i].S,
+        };
+        if (currentStringified !== JSON.stringify(traces[y][i])) {
           fringe.add(`${i},${y}`);
         }
       }
     }
     if (state.N !== undefined) {
-      let value = state.N;
-      for (let i = y - 1; i >= Math.max(0, y - 4); i--) {
+      let { value, trace: currentTrace } = state.N;
+      for (let i = y - 1; i > Math.max(-1, y - 4); i--) {
         value += map[i][x];
-        if (traces[i][x].E === undefined || value < traces[i][x].E!) {
-          traces[i][x].E = value;
-          fringe.add(`${x},${i}`);
-        }
-        if (traces[i][x].W === undefined || value < traces[i][x].W!) {
-          traces[i][x].W = value;
+        const currentStringified = JSON.stringify(traces[i][x]);
+        traces[i][x] = {
+          ...traces[i][x],
+          E:
+            value < (traces[i][x].E?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(y - i, "N"),
+                }
+              : traces[i][x].E,
+          W:
+            value < (traces[i][x].W?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(y - i, "N"),
+                }
+              : traces[i][x].W,
+        };
+        if (currentStringified !== JSON.stringify(traces[i][x])) {
           fringe.add(`${x},${i}`);
         }
       }
     }
     if (state.S !== undefined) {
-      let value = state.S;
+      let { value, trace: currentTrace } = state.S;
       for (let i = y + 1; i < Math.min(map.length, y + 4); i++) {
         value += map[i][x];
-        if(traces[i][x].E === undefined || value < traces[i][x].E!) {
-          traces[i][x].E = value;
-          fringe.add(`${x},${i}`);
-        }
-        if (traces[i][x].W === undefined || value < traces[i][x].W!) {
-          traces[i][x].W = value;
+        const currentStringified = JSON.stringify(traces[i][x]);
+        traces[i][x] = {
+          ...traces[i][x],
+          E:
+            value < (traces[i][x].E?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(i - y, "S"),
+                }
+              : traces[i][x].E,
+          W:
+            value < (traces[i][x].W?.value ?? Infinity)
+              ? {
+                  value,
+                  trace: currentTrace + repeat(i - y, "S"),
+                }
+              : traces[i][x].W,
+        };
+        if (currentStringified !== JSON.stringify(traces[i][x])) {
+          // fringeMap.set(`${x},${i}`, fringeMap.get(firstElem)! + "S");
           fringe.add(`${x},${i}`);
         }
       }
     }
     // console.log(fringe);
     // console.log(traces);
-    const lastElem =  JSON.stringify(traces[map.length - 1][map[0].length - 1]);
-    if (prev !== lastElem) {
-      console.log(lastElem);
-      prev = lastElem;
-    }
+    // const lastElem = JSON.stringify(traces[map.length - 1][map[0].length - 1]);
+    // if (prev !== lastElem) {
+    //   console.log(lastElem);
+    //   // console.log('Last elem trace', fringeMap.get(`${map[0].length - 1},${map.length - 1}`))
+    //   prev = lastElem;
+    // }
+    // const lastElem = traces[map.length - 1][map[0].length - 1];
+    // Object.values(lastElem).forEach(({ value, trace }) => {
+    //   if (value === 1128) {
+    //     const coords = traceToCorrds(trace);
+    //     console.log("\n\n")
+    //     // printTrace(map, coords);
+    //     console.log(trace);
+
+    //     console.log("\n\n")
+    //   }
+    // });
   }
-  console.log('Last one', traces[map.length - 1][map[0].length - 1]);
-  return Math.min(...Object.values(traces[map.length - 1][map[0].length - 1]));
+  // console.log("Last one", traces[map.length - 1][map[0].length - 1]);
+  // console.log(fringeMap)
+
+  let min = Infinity;
+  let minTrace = "";
+  [...Object.values(traces[map.length - 1][map[0].length - 1])].forEach(
+    ({ value, trace }) => {
+      if (value < min) {
+        min = value;
+        minTrace = trace;
+      }
+    }
+  );
+
+  // const coords = traceToCorrds(minTrace);
+  // console.log(minTrace);
+  // printTrace(map, coords);
+
+  return Math.min(
+    ...[...Object.values(traces[map.length - 1][map[0].length - 1])].map(
+      ({ value }) => value
+    )
+  );
 };
 
-console.log('First part:', first(parse(input)));
+console.log("First part:", first(parse(input)));
 // console.log('First part:', first(parse(input)));
